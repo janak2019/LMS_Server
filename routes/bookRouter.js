4e0a4515599218f5bcb8dc3898d970459758816d
htmlbasic <withjanak@gmail.com>
2025-09-22 20:23:58 +0545

    authenticated removed from book router for public book access

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 
 router.post("/admin/add",isAuthenticated, isAuthorized("Admin"), addBook);
-router.get("/all", isAuthenticated, getAllBooks);   
+router.get("/all", getAllBooks);   
 router.delete("/delete/:id", isAuthenticated, isAuthorized("Admin"), deleteBook);
 
 
