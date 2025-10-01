ae11a705a4cf5203dfcfee4caa9328a5dd8fc9c1
htmlbasic <withjanak@gmail.com>
2025-10-01 13:06:06 +0545

    secure changed to true in sendToken.js

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -5,7 +5,7 @@ export const sendToken = (user, statusCode, message, res) => {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: "None",
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
