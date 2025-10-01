ba1d018e61f62bc0eb346045a713eeeaaf4367d9
htmlbasic <withjanak@gmail.com>
2025-10-01 13:02:47 +0545

    same site strict to none  in send Token.js

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -4,7 +4,7 @@ export const sendToken = (user, statusCode, message, res) => {
   const options = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "None",
     secure: process.env.NODE_ENV === "production",
   };
 
